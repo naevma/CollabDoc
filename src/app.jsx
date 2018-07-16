1b0fadf17fa3680d52db318163e2f8ac1fed21b8
git status <patrick.combe@duke.edu>
2018-07-16 11:13:33 -0700

    second commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import {Editor, EditorState} from 'draft-js';
+
+import Draft from './component/draft';
+
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
-  }
+
   render() {
     return (
       <div id="content">
-        <h1>Draft.js Editor</h1>
-        <div className="editor">
-          <Editor
-            editorState={this.state.editorState}
-            onChange={this.onChange}
-          />
-        </div>
+        <Draft />
       </div>
     );
   }
